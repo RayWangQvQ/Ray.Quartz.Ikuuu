5daae94af8824358e0245c9efa2f29565b7c31b1
Ray <zai7lou@outlook.com>
2023-03-31 02:04:48 +0800

    update

--- src/Ray.Quartz.Ikuuu/Agents/CookieHttpClientHandler.cs
+++ src/Ray.Quartz.Ikuuu/Agents/CookieHttpClientHandler.cs
@@ -12,12 +12,10 @@ namespace Ray.Quartz.Ikuuu.Agents
     {
         private readonly ILogger<CookieHttpClientHandler> _logger;
         private readonly CookieManager _ckManager;
-        private readonly CookieInfo _ckInfo;
 
         public CookieHttpClientHandler(
             ILogger<CookieHttpClientHandler> logger, 
-            CookieManager ckManager, 
-            CookieInfo ckInfo
+            CookieManager ckManager
             )
         {
             _logger = logger;
